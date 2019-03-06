18225c9cf5176ade81c1f009cfc948b168ba424d
Christopher Covert <covertops5@gmail.com>
2019-03-06 18:38:58 -0500

    :sparkles: Auto construct/deconstruct ghosts

--- control.lua
+++ control.lua
@@ -8,16 +8,13 @@ script.on_init(function()
     init_designer_list()
 end)
 
-script.on_event(defines.events.on_player_created, function(ev)
-end)
-
 script.on_event('bpd-toggle-designers-list', function(ev)
     toggle_designer_list(game.players[ev.player_index])
 end)
 
 script.on_event('bpd-enter-designer', function(ev)
     local player = game.players[ev.player_index]
-
+    
     if is_in_designer(player) then
         exit_designer(player)
     else
@@ -32,7 +29,7 @@ script.on_event(defines.events.on_gui_click, function(ev)
         hide_designer_list(player)
     elseif ev.element.name == 'blueprint-designer-create-designer' then
         local name = designer_list_get_name_text(player)
-
+        
         if name == '' or name == nil then
             player.print('You need to enter a designer name', {r = 1, g = 0, b = 0, a = 1})
             return
@@ -51,4 +48,26 @@ script.on_event(defines.events.on_gui_click, function(ev)
         local name = remove_prefix(ev.element.name, 'blueprint-designer-list-delete-')
         delete_designer(player, name)
     end
+end)
+
+script.on_event(defines.events.on_built_entity, function(ev)
+    local player = game.players[ev.player_index]
+    if not is_in_designer(player) then return end
+    
+    local type = ev.created_entity.type
+    if type == 'entity-ghost' or type == 'tile-ghost' then
+        player.print('Reviving...')
+        ev.created_entity.revive()
+    end
+    
+    if type == 'lab' then
+        ev.created_entity.active = false
+    end
+end)
+
+script.on_event(defines.events.on_marked_for_deconstruction, function(ev)
+    local player = game.players[ev.player_index]
+    if not is_in_designer(player) then return end
+    
+    ev.entity.destroy()
 end)

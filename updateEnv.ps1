e0c067d01b81427b6d99789b8e1d92488059a5fd
Christopher Covert <covertops5@gmail.com>
2019-03-13 18:02:19 -0400

    :wrench: Improve env script

--- updateEnv.ps1
+++ updateEnv.ps1
@@ -21,7 +21,7 @@ $version_regex = '("version"\s*:\s*")((?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+
 
 $version_info = Get-Content .\info.json |
     # %{$_ -split "\n"} |
-    %{if ($_ -match $version_regex) { [PSCustomObject]$Matches }} |
+    ForEach-Object {if ($_ -match $version_regex) { [PSCustomObject]$Matches }} |
     Select-Object -Property @{Name="Major"; Expression = {[int]($_.Major)}},
                             @{Name="Minor"; Expression = {[int]($_.Minor)}},
                             @{Name="Patch"; Expression = {[int]($_.Patch)}}
@@ -30,10 +30,16 @@ Write-Host "Version: $($version_info.Major).$($version_info.Minor).$($version_in
 
 if ($BumpMajor -or $BumpMinor -or $BumpPatch) {
     Write-Host "Performing Version Bump"
-    if ($BumpMajor) { $version_info.Major = $version_info.Major + 1 }
-    if ($BumpMinor) { $version_info.Minor = $version_info.Minor + 1 }
-    if ($BumpPatch) { $version_info.Patch = $version_info.Patch + 1 }
-    # $version_info = 
+    if ($BumpMajor) {
+        $version_info.Major = $version_info.Major + 1
+        $version_info.Minor = 0
+        $version_info.Patch = 0
+    } elseif ($BumpMinor) {
+        $version_info.Minor = $version_info.Minor + 1
+        $version_info.Patch = 0
+    } elseif ($BumpPatch) {
+        $version_info.Patch = $version_info.Patch + 1
+    }
 
     Write-Host "New Version: $($version_info.Major).$($version_info.Minor).$($version_info.Patch)"
 
